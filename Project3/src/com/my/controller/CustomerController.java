56a09975e6973a877102343aba52832fe5a8b446
Jinseob\Jinseob Bae <bbb1125@naver.com>
2017-07-03 16:54:07 +0900

    컨트롤러 수정

--- Project3/src/com/my/controller/CustomerController.java
+++ Project3/src/com/my/controller/CustomerController.java
@@ -2,12 +2,10 @@ package com.my.controller;
 
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
+import java.util.regex.Pattern;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.my.dao.BoardDAOOracle;
-import com.my.dao.CustomerDAO;
 import com.my.dao.CustomerDAOOracle;
 import com.my.vo.Customer;
-import com.my.vo.RepBoard;
 
 @Controller
 public class CustomerController {
@@ -146,66 +141,69 @@ public class CustomerController {
 		return "redirect:main.jsp";
 		
 	}
-	
-	@RequestMapping(value= "/signup.do", method=RequestMethod.POST)
-	public String signup(Model model, String email, String name, String nick, String pwd, String repwd, String tel, String tel1, String tel2){
-		String msg="";
-		String status = "a";
+	   @RequestMapping(value= "/signup.do", method=RequestMethod.POST)
+	   public String signup(Model model, String email, String name, String nick, String pwd, String repwd, String tel, String tel1, String tel2){
+	      String msg="";
+	      String status = "a";
 
-		if( email == null || nick == null || pwd == null || repwd == null || tel == null || tel1 == null || tel2 == null){
-			msg = "2";
-		} else {
+	      if( email.equals("") || nick.equals("") || pwd.equals("") || repwd.equals("") || tel.equals("") || tel1.equals("") || tel2.equals("")){
+	         msg = "2";
+	      } else{
+	               try {
+	                  String num = tel+"-"+tel1+"-"+tel2;
+	                  Customer c = new Customer();
+	                  c.setEmail(email);
+	                  c.setCustomer_name(name);
+	                  c.setNickname(nick);
+	                  c.setPassword(pwd);
+	                  c.setStatus(status);
+	                  c.setCall_no(num);
+	                  dao.singup(c);
+	                  msg = "1";
+	               } catch (Exception e) {
+	                  e.printStackTrace();
+	               }
+	            }
+	      
+	      model.addAttribute("msg", msg);
+	      
+	      String forwradURL="/result.jsp";
+	      return forwradURL;
+	   }
 
-			try {
-				String num = tel+"-"+tel1+"-"+tel2;
-				Customer c = new Customer();
-				c.setEmail(email);
-				c.setCustomer_name(name);
-				c.setNickname(nick);
-				c.setPassword(pwd);
-				c.setStatus(status);
-				c.setCall_no(num);
-				dao.singup(c);
-				msg = "1";
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		model.addAttribute("msg", msg);
-		
-		String forwradURL="/result.jsp";
-		return forwradURL;
-	}
-
-	@RequestMapping("/dupchkid.do")
-	public String dupchkid(String email, Model model)
-			throws IOException, ServletException {
+	   @RequestMapping("/dupchkid.do")
+	   public String dupchkid(String email, Model model)
+	         throws IOException, ServletException {
 
 
-		String msg = "";
+	      String msg = "";
 
 
-		Customer c;
-		try {
-			c = dao.selectById(email);	
-			if(c == null){
-				msg = "1";		//selectById한값에 id가 없을 경우 1을 전송
-			} else {
-				msg = "-1";		//그 반대.
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	      Customer c;
+	      try {
+	         c = dao.selectById(email);
+	         if (!email.contains("@")){
+	            msg = "2";
+	         }else if(Pattern.matches("[가-힣]", email)){
+	            msg ="3";
+	         }else if(c == null){
+	            msg = "1";      //selectById한값에 id가 없을 경우 1을 전송
+	         } else {
+	            msg = "-1";      //그 반대.
+	         }
+	         
+	      } catch (ClassNotFoundException e) {
+	         e.printStackTrace();
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
 
-		
+	      
 
-		model.addAttribute("msg", msg); //다른객체가 쓸 수 있게 메모리영역에 저장
-		String forwardURL="result.jsp";
-		return forwardURL;
-	}
+	      model.addAttribute("msg", msg); //다른객체가 쓸 수 있게 메모리영역에 저장
+	      String forwardURL="result.jsp";
+	      return forwardURL;
+	   }
 	
 	
 	
